2e756667f3eb9354ecbdff93ec20c41697048118
cod0216 <cod0216@nate.com>
2025-06-01 16:07:06 +0900

    js study

--- section03/src/index.js
+++ section03/src/index.js
@@ -17,9 +17,9 @@ import mul, { add, sub } from "./math.js";
 // default로 export 하면 중괄호 없이 import 해야 된다.
 // 그리고 default한건 import 시 마음대로 이름을 정해서 불러올 수 있다.
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
 
 // node 파일경로.js --> 실행
 
@@ -27,3 +27,11 @@ console.log(mul(1, 2));
 /*
 module.exports{ 만든 함수 : 함수 } --> require('/경로');
 */
+
+import randomColor from "randomcolor";
+// randomColor라이브러리가 내보낸 randomColor를 불러올 수 있다.
+// 경로가 아닌 이름만 명시해줘도 괜찮다.
+
+const color = randomColor();
+
+console.log(color);
